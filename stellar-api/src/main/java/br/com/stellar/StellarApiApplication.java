9ed1f39ac455abbd9550c56879d9055947156caa
carlosedupm <carlosedupm@gmail.com>
2020-11-02 13:22:03 -0300

    Criação da estrutura do projeto.

---
+++ stellar-api/src/main/java/br/com/stellar/StellarApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.stellar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StellarApiApplication.class, args);
+	}
+
+}
